0a2f3c720e68bd40d8706e38a728719404f31cdb
Ramiz Ahmed <ramizahmedar@gmail.com>
2023-06-22 02:19:04 +0500

    Add reserve functionality

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,5 +1,6 @@
 import getCurrentUser from "@/actions/getCurrentUser";
 import getListingById from "@/actions/getListingById";
+import getReservations from "@/actions/getReservations";
 import Container from "@/components/Container";
 import EmptyState from "@/components/EmptyState";
 import ListingClient from "./components/ListingClient";
@@ -11,12 +12,17 @@ interface IParams {
 export default async function ListingPage({ params }: { params: IParams }) {
   const listing = await getListingById(params);
   const user = await getCurrentUser();
+  const reservations = await getReservations(params);
 
   if (!listing) return <EmptyState />;
 
   return (
     <Container>
-      <ListingClient listing={listing} user={user} />
+      <ListingClient
+        reservations={reservations}
+        listing={listing}
+        currentUser={user}
+      />
     </Container>
   );
 }
