dc704c58eef01588bded51d3f86034d392125a81
Ramiz Ahmed <ramiz.ahmed@letspingup.com>
2025-03-25 22:37:46 +0500

    Fix title

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -10,9 +10,9 @@ export default function Heading({
   center = false,
 }: HeadingProps) {
   return (
-    <div className={`${center ? "text-center" : "text-start"}`}>
-      <div className="text-2xl font-bold">{title}</div>
-      <div className="font-light text-neutral-500 mt-2">{subtitle}</div>
+    <div className={`mt-[100px] ${center ? 'text-center' : 'text-start'}`}>
+      <div className='text-2xl font-bold'>{title}</div>
+      <div className='font-light text-neutral-500 mt-2'>{subtitle}</div>
     </div>
   );
 }
